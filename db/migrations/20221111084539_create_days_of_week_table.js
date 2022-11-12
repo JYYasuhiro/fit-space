de58b72cbaaf180c992578fbb44537d22f999de9
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-12 10:43:46 +0900

    add week_table migration and seed

---
+++ db/migrations/20221111084539_create_days_of_week_table.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    return await knex.schema.createTable('week_table', (table) => {
+        table.increments('id').primary();
+        table.string('day').unique().notNullable();
+    })
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTable('week_table');
+};
