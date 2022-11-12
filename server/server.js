f4030ef2722343ac41e301e1a25ad559d5905ebc
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-12 15:13:37 +0900

    feat: add GET week day endpoint

--- server/server.js
+++ server/server.js
@@ -10,19 +10,33 @@ function setupServer() {
 
     app.get('/hello', async (req,res) => {
         res.status(200).send('🍎 successful test get endpoint')
-   
+   });
     //indicates what body part an exercise will be hitting, by exercise_type ID 
-    app.get('/:id/type', async (req,res) => {
+    app.get('/type/:id', async (req,res) => {
       const id = req.params.id;
       try{
-        const table = await db('exercise_type').where('id', id).select('*')
-        console.log(table);
+        const table = await db('exercise_type')
+            .where('id', id)
+            .select('*');
         res.status(200).send(table[0].name)
       } catch(err) {
         res.status(500).send(err);
       }
-    })
     });
+    //returns the day of the week, week_table ID
+    app.get('/week/:id', async (req,res) => {
+        const id = req.params.id;
+        try {
+            const table = await db('week_table')
+                .where('id', id)
+                .select('*')
+                console.log(table[0].day);
+            res.status(200).send(table[0].day);
+        } catch(err) {
+            res.status(500).send(err);
+        }
+    })
+    
     return app;
 }
 
