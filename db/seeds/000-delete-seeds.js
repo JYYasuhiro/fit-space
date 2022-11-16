ee519217b221a0f923ba7c0eec82e18b4b1d82b0
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2022-11-16 10:25:50 +0900

    fix: change order of 000-delete-seeds

--- db/seeds/000-delete-seeds.js
+++ db/seeds/000-delete-seeds.js
@@ -4,9 +4,8 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  // await knex('workout_table').del()
+  await knex('workout_table').del()
   await knex('exercise_table').del()
   await knex('week_table').del()
   await knex('exercise_type').del()
-  await knex('workout_table').del()
 };
