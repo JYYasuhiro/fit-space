5fcbc962e9fa6bd885fa826da1df7e6b3bcb7015
joeyYY <88700081+JYYasuhiro@users.noreply.github.com>
2023-01-08 17:39:07 +0900

    feat: add sidenav to components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,29 +5,31 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-
+import '../styles/navbar.css'
 export default function Navbar() {
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" maxwidth="lg">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-  
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-           Fit Space   
-           <FitnessCenterIcon sx ={{margin: 2}}/>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div className='navbar__container'>
+      <Box sx={{ height: '100px' }}>
+        <AppBar position="static" maxwidth="lg">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Fit Space
+            <FitnessCenterIcon sx ={{margin: 2}}/>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+    
 
   )
 }
